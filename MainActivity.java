18d917c396891943eb86253165a9b1d167a58c89
sushil.verma <sushilverma1985@gmail.com>
2017-02-13 14:43:39 +0530

    b

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sushilverma.thirdgit;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "hello ", Toast.LENGTH_SHORT).show();
+
+
     }
 }
